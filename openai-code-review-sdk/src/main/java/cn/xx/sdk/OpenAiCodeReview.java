2b5b5fb37a9c4bc933a500e479b2c25d6bd72ded
Xiaoxin0430 <2878647251@qq.com>
2026-06-19 21:12:13 +0800

    feat:检出代码

--- openai-code-review-sdk/src/main/java/cn/xx/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/xx/sdk/OpenAiCodeReview.java
@@ -1,5 +1,10 @@
 package cn.xx.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author xiaoxin
  * @description
@@ -8,7 +13,27 @@ package cn.xx.sdk;
 
 
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("请注意倒车");
+
+        //代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder sb = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+
+        int exitCode = process.exitValue();
+        System.out.println("Exited with code " + exitCode);
+
+        System.out.println("评审代码" + sb.toString());
+
     }
 }
--- openai-code-review-sdk/src/test/java/cn/xx/sdk/test/ApiTest.java
+++ openai-code-review-sdk/src/test/java/cn/xx/sdk/test/ApiTest.java
@@ -9,6 +9,6 @@ package cn.xx.sdk.test;
 
 public class ApiTest {
     public static void main(String[] args) {
-
+        System.out.println(Integer.parseInt("aaaa"));
     }
 }
