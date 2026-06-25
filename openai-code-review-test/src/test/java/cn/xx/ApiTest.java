362b242ed784f12b1d4bd411f101f9adf37ad637
Xiaoxin0430 <2878647251@qq.com>
2026-06-25 16:34:55 +0800

    feat:微信推送

--- openai-code-review-test/src/test/java/cn/xx/ApiTest.java
+++ openai-code-review-test/src/test/java/cn/xx/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("abcdefghijk1234"));
+        System.out.println(Integer.parseInt("abcdefghijkl1234"));
     }
 
 }
